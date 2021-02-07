7b8ac4c38e93e0c031692e178fa20478a8d93e0c
Jake Heft <jakeheft@gmail.com>
2021-02-06 19:18:13 -0700

    Add annotations to image mutations

---
+++ code/api/src/modules/image/mutations.js
@@ -0,0 +1,6 @@
+// import graphql
+// import ProductType
+// import resolver actions
+
+// create a mutation to add image (id--Int, image path--string, category--string, style tag--string)
+// create a mutation to remove image
