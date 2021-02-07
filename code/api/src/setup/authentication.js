ee22817f8b273547606f8545fe5551019215abfb
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-02-07 14:22:36 -0700

    Annotate authentication.js

--- code/api/src/setup/authentication.js
+++ code/api/src/setup/authentication.js
@@ -3,10 +3,12 @@ import jwt from 'jsonwebtoken'
 import serverConfig from '../config/server.json'
 
 // Authentication middleware
+// This function logs in a user for a single request from the auth token
 export default function (request, response, next) {
   let authToken = request.headers.authorization
   debugger;
   if (authToken && authToken !== null) {
+    // If there is a valid auth token, use it to add a user object to the request
     try {
       const token = authToken.split(' ')
       request.user = jwt.verify(token[1], serverConfig.secret)
@@ -14,8 +16,9 @@ export default function (request, response, next) {
       console.warn('Invalid token detected.')
     }
   } else {
+    // Otherwise, the request user is empty
     request.user = {}
   }
-
+  // Move along
   next()
 }
