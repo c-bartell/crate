7ced4faca902957f86b15b9873785c759a9d719b
Jake Heft <jakeheft@gmail.com>
2021-02-06 19:26:26 -0700

    Add image resolvers file with annotations

---
+++ code/api/src/modules/image/resolvers.js
@@ -0,0 +1,6 @@
+// import params
+// import models
+
+// add a create resolver to create an image
+// add a remove resolver to remove an image
+// add resolver function that gets images based on a provided category type
