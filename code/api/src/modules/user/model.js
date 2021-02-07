3ab94b713407d6373d7a33c17e0fcbabde9faf46
PhilipDeFraties <philipjfry38@gmail.com>
2021-02-06 17:00:14 -0700

    user model annotations

--- code/api/src/modules/user/model.js
+++ code/api/src/modules/user/model.js
@@ -1,6 +1,7 @@
 'use strict'
 
 // User
+// defines user model
 module.exports = function(sequelize, DataTypes) {
   let User = sequelize.define('users', {
     name: {
@@ -16,10 +17,10 @@ module.exports = function(sequelize, DataTypes) {
       type: DataTypes.TEXT
     }
   })
-
+  // defines relationship hasmany for subscriptions
   User.associate = function(models) {
     User.hasMany(models.Subscription)
   }
-
+  // returns user object
   return User
 }
