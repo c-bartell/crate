d8696531de17b26e26e9348883cdfa17d09744cf
Jake Heft <jakeheft@gmail.com>
2021-02-06 18:08:32 -0700

    Add annotations to user mutations

--- code/api/src/modules/user/mutations.js
+++ code/api/src/modules/user/mutations.js
@@ -1,11 +1,15 @@
 // Imports
+// this line will allow for GraphQL to be used, pulled from built in 'graphql' module. I believe this is specifically using predefined graphql data types
 import { GraphQLString, GraphQLInt } from 'graphql'
 
 // App Imports
+// this imports the UserType class, which is a GraphQLObjectType instance. It sets the available fields for a user
 import { UserType } from './types'
+// this loads in create and remove resolver functions, which are called in the userSignup and userRemove mutations below. These async functions instruct how to create and remove users, with the create also checking a user already exists.
 import { create, remove } from './resolvers'
 
 // Create
+// This will allow a new user to be created, requires name, email, password
 export const userSignup = {
   type: UserType,
   args: {
@@ -28,6 +32,7 @@ export const userSignup = {
 }
 
 // Remove
+// this will allow a user to be deleted based on user id provided
 export const userRemove = {
   type: UserType,
   args: {
