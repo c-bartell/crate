9d786548eac7812b17d9f8b276deef494daabebd
Curtis Bartell <60277914+c-bartell@users.noreply.github.com>
2021-02-07 13:45:01 -0700

    Add annotations to load-modules.js

--- code/api/src/setup/load-modules.js
+++ code/api/src/setup/load-modules.js
@@ -1,4 +1,4 @@
-// Imports
+ // Imports
 import express from 'express'
 import path from 'path'
 import cors from 'cors'
@@ -7,6 +7,7 @@ import cookieParser from 'cookie-parser'
 import morgan from 'morgan'
 
 // App Imports
+// Importing the NODE_ENV variable tells the server how to behave when running in different environments.
 import { NODE_ENV } from '../config/env'
 
 // Load express modules
@@ -14,13 +15,21 @@ export default function (server) {
   console.info('SETUP - Loading modules...')
 
   // Enable CORS
+  // Tells the server to use Cross-Origin Resource Sharing
   server.use(cors())
 
   // Request body parser
+  // Like JSON.parse, this adds tools to the server to convert the raw json in a
+  // request body to an object.
   server.use(bodyParser.json())
   server.use(bodyParser.urlencoded({ extended: false }))
 
   // Request body cookie parser
+  // If I'm understanding the docs correctly, cookieParser allows the api to
+  // interpret any cookies passed in with a request. This is odd because, on the
+  // front end, the user token is taken out of the cookie and passed as an auth
+  // header, rather than passing the whole cookie in API requests. As far as I
+  // can tell, this is never used.
   server.use(cookieParser())
 
   // Static files folder
