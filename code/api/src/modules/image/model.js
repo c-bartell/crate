3cc74a41bdde3d641547866bae44e53fe65abdb2
Jake Heft <jakeheft@gmail.com>
2021-02-06 19:10:39 -0700

    Add annotations for image category

--- code/api/src/modules/image/model.js
+++ code/api/src/modules/image/model.js
@@ -1,1 +1,1 @@
-// to alter: this file will allow us to define an Image that will contain an id, image path, and style tag. An image will not have any relationship to another table
+// to alter: this file will allow us to define an Image that will contain an id, image path, image category and style tag. An image will not have any relationship to another table
