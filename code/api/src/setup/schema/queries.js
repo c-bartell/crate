af128b5ee76081a8b9c95cb5cb329f85e804a566
Jake Heft <jakeheft@gmail.com>
2021-02-06 19:05:32 -0700

    Add annotations to schema queries

--- code/api/src/setup/schema/queries.js
+++ code/api/src/setup/schema/queries.js
@@ -1,13 +1,13 @@
 // Imports
 import { GraphQLObjectType } from 'graphql'
 
-// App Imports
+// App Imports - imports queries from each model
 import * as user from '../../modules/user/query'
 import * as product from '../../modules/product/query'
 import * as crate from '../../modules/crate/query'
 import * as subscription from '../../modules/subscription/query'
 
-// Query
+// Query - sets models available to be queried
 const query = new GraphQLObjectType({
   name: 'query',
   description: 'API Queries [Read]',
@@ -21,3 +21,5 @@ const query = new GraphQLObjectType({
 })
 
 export default query
+
+// to alter: import image queries and add inside of query constant
