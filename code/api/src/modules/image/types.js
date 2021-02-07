207f82d45cb435cfbb2a057b97a0867ece3d6592
Jake Heft <jakeheft@gmail.com>
2021-02-06 19:31:44 -0700

    Add image type file with annotations

---
+++ code/api/src/modules/image/types.js
@@ -0,0 +1,5 @@
+// import graphql types
+
+// add ImageType class which inerhits from GraphQLObjectType, set fields to id (int), image path (string), category type (string), and style tag (string)
+// add ImageCategoryType which takes either an id or a category type
+// export image types
