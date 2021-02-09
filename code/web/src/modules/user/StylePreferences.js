27e309b43e8cde74b6f9f19b4c36a443c3ff111a
Bryce Mara Jarrett <brycej98@gmail.com>
2021-02-09 16:57:38 -0600

    Add Redux Dev tools and imports

--- code/web/src/modules/user/StylePreferences.js
+++ code/web/src/modules/user/StylePreferences.js
@@ -1,12 +1,33 @@
-import React from 'react'
+import React, { Component } from "react";
+// import { connect } from 'react-redux'
+import { H3 } from "../../ui/typography";
+// import { Grid, GridCell } from '../../ui/grid'
+// import { grey, grey2 } from '../../ui/common/colors'
+// import { getListByUser } from '../subscription/api/actions'
+import { Link, withRouter } from "react-router-dom";
 
-const StylePreferences = () => {
+class StylePreferences extends Component {
+  constructor(props) {
+    super(props);
 
-  return(
-    <section>
-      <h1>Style Survey Page</h1>
-    </section>
-  )
+    this.state = {
+      style: "",
+    };
+  }
+
+  render() {
+    return (
+      <section>
+        <Grid style={{ backgroundColor: grey }}>
+          <GridCell style={{ padding: '2em', textAlign: 'center' }}>
+             <H3 font="secondary">Style Survey</H3>
+            <p style={{ marginTop: '1em', color: grey2 }}>Fill out the survey</p>
+          </GridCell>
+        </Grid>
+      </section>
+    );
+  }
 }
 
 export default StylePreferences;
+
