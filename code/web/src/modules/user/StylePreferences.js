c30df7e7cfd788b0b7d15bd1c5768de90d637cdd
Bryce Mara Jarrett <brycej98@gmail.com>
2021-02-09 15:47:58 -0600

    Add updated route

---
+++ code/web/src/modules/user/StylePreferences.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const StylePreferences = () => {
+
+  return(
+    <section>
+      <h1>Style Survey Page</h1>
+    </section>
+  )
+}
+
+export default StylePreferences;
