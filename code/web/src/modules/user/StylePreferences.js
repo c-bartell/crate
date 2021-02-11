0de98c4b934a925427031dc07cdd42a5a088cebb
Estelle Staffieri <estellelareinne@gmail.com>
2021-02-11 15:56:45 -0700

    Update style survey to be gender neutral

--- code/web/src/modules/user/StylePreferences.js
+++ code/web/src/modules/user/StylePreferences.js
@@ -12,12 +12,13 @@ class StylePreferences extends Component {
     super(props);
 
     this.state = {
-      tops: "",
-      bottoms: "",
-      accessories: "",
-      shoes: "",
+      vacation: "",
+      flavors: "",
+      restaurants: "",
+      architecture: "",
     };
   }
+
 
   onSelect = (value, kind) => {
     this.setState({ [kind]: value });
@@ -26,13 +27,13 @@ class StylePreferences extends Component {
   onSubmit() {
   
     const selectionString =
-      this.state.tops +
+      this.state.vacation +
       " " +
-      this.state.bottoms +
+      this.state.flavors +
       " " +
-      this.state.accessories +
+      this.state.restaurants +
       " " +
-      this.state.shoes;
+      this.state.architecture;
     //send this string to the backend! 
 
     // bring up the modal with the style summary! 
@@ -45,7 +46,8 @@ class StylePreferences extends Component {
           <GridCell style={{ padding: "2em", textAlign: "center" }}>
             <H3 font="secondary">Style Survey</H3>
             <p style={{ marginTop: "1em", color: grey2 }}>
-              To fill out the survey please select one item from each category that most appeals to you.
+              Thank you for subscribing! Here is a short survey so we can tailor your crate to you!
+              To fill out the survey please select one image from each category and click submit.
             </p>
           </GridCell>
         </Grid>
@@ -56,51 +58,51 @@ class StylePreferences extends Component {
             textAlign: "center",
           }}
         >
-          <H3 font="secondary">Which top best represents your style?</H3>
+          <H3 font="secondary">Favorite vacation destination?</H3>
           <div style={{ margin: "2em", display: "flex", flexDirection: "row" }}>
             <SurveyCards
               images={[
                 { id: "casual", src: "red" },
                 { id: "classy", src: "red" },
-                { id: "sketchy", src: "red" },
+                { id: "edgy", src: "red" },
               ]}
-              category={"tops"}
+              category={"vacation"}
               onSelect={this.onSelect}
             />
           </div>
-          <H3 font="secondary">Which bottom best represents your style?</H3>
+          <H3 font="secondary">Are more a spicy, sweet or sour person?</H3>
           <div style={{ margin: "2em", display: "flex", flexDirection: "row" }}>
             <SurveyCards
               images={[
                 { id: "casual", src: "red" },
                 { id: "classy", src: "red" },
-                { id: "sketchy", src: "red" },
+                { id: "edgy", src: "red" },
               ]}
-              category={"bottoms"}
+              category={"flavors"}
               onSelect={this.onSelect}
             />
           </div>
-          <H3 font="secondary">Which accessories best represent your style?</H3>
+          <H3 font="secondary">You're hungry, what restaurant do you go to?</H3>
           <div style={{ margin: "2em", display: "flex", flexDirection: "row" }}>
             <SurveyCards
               images={[
                 { id: "casual", src: "red" },
                 { id: "classy", src: "red" },
-                { id: "sketchy", src: "red" },
+                { id: "edgy", src: "red" },
               ]}
-              category={"accessories"}
+              category={"restaurants"}
               onSelect={this.onSelect}
             />
           </div>
-          <H3 font="secondary">Which shoes best represent your style?</H3>
+          <H3 font="secondary">Which would you pick as your dream home?</H3>
           <div style={{ margin: "2em", display: "flex", flexDirection: "row" }}>
             <SurveyCards
               images={[
                 { id: "casual", src: "red" },
                 { id: "classy", src: "red" },
-                { id: "sketchy", src: "red" },
+                { id: "edgy", src: "red" },
               ]}
-              category={"shoes"}
+              category={"architecture"}
               onSelect={this.onSelect}
             />
           </div>
@@ -117,4 +119,6 @@ class StylePreferences extends Component {
   }
 }
 
-export default StylePreferences;
+
+export default StylePreferences
+
