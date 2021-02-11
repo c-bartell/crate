c8a00467fc04fcbb8f3263986e0b15c541a91a6a
Bryce Mara Jarrett <brycej98@gmail.com>
2021-02-10 18:39:40 -0600

    Add successfully functionality

--- code/web/src/modules/user/StylePreferences.js
+++ code/web/src/modules/user/StylePreferences.js
@@ -1,59 +1,118 @@
 import React, { Component } from "react";
-// import { connect } from 'react-redux'
 import { H3 } from "../../ui/typography";
-import { Grid, GridCell } from '../../ui/grid'
-import { white, grey, grey2 } from '../../ui/common/colors'
+import { Grid, GridCell } from "../../ui/grid";
+import { white, grey, grey2 } from "../../ui/common/colors";
 
-import Button from '../../ui/button/Button'
-import Icon from '../../ui/icon'
+import Button from "../../ui/button/Button";
+import Icon from "../../ui/icon";
 import SurveyCards from "./SurveyCards";
-
 
 class StylePreferences extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      style: "",
+      tops: "",
+      bottoms: "",
+      accessories: "",
+      something: "",
     };
+  }
+
+  onSelect = (value, kind) => {
+    this.setState({ [kind]: value });
+  };
+
+  onSubmit() {
+  
+    const selectionString =
+      this.state.tops +
+      " " +
+      this.state.bottoms +
+      " " +
+      this.state.accessories +
+      " " +
+      this.state.something;
+    //send this string to the backend! 
   }
 
   render() {
     return (
       <section>
         <Grid style={{ backgroundColor: grey }}>
-          <GridCell style={{ padding: '2em', textAlign: 'center' }}>
-             <H3 font="secondary">Style Survey</H3>
-            <p style={{ marginTop: '1em', color: grey2 }}>Fill out the survey</p>
+          <GridCell style={{ padding: "2em", textAlign: "center" }}>
+            <H3 font="secondary">Style Survey</H3>
+            <p style={{ marginTop: "1em", color: grey2 }}>
+              Fill out the survey
+            </p>
           </GridCell>
         </Grid>
-        <Grid>
-          <GridCell>
-            <H3 font="secondary">Which top best represents your style?</H3>
-            <div style={{ margin: '2em', display: "flex", flexDirection: "row" }}>
-              <SurveyCards props={["White", "Blue", "Yellow"]} />
-            </div>
-            <H3 font="secondary">Which bottom best represents your style?</H3>
-            <div style={{ margin: '2em', display: "flex", flexDirection: "row" }}>
-              <SurveyCards props={["Green", "Pink", "Orange"]} />
-            </div>
-            <H3 font="secondary">Which accecories best represent your style?</H3>
-            <div style={{ margin: '2em', display: "flex", flexDirection: "row" }}>
-              <SurveyCards props={["Hello", "Hi", "Hey"]} />
-            </div>
-            <H3 font="secondary">Which 'something' best represent your style?</H3>
-            <div style={{ margin: '2em', display: "flex", flexDirection: "row" }}>
-              <SurveyCards props={["Yes", "Yeah", "Perfect"]} />
-            </div>
-          </GridCell>
-        </Grid>
-        <Button type="button" theme="primary">
-          <Icon size={1.2} style={{ color: white }}>add</Icon> Submit Survey
-        </Button>
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            textAlign: "center",
+          }}
+        >
+          <H3 font="secondary">Which top best represents your style?</H3>
+          <div style={{ margin: "2em", display: "flex", flexDirection: "row" }}>
+            <SurveyCards
+              images={[
+                { id: "casual", src: "red" },
+                { id: "classy", src: "red" },
+                { id: "sketchy", src: "red" },
+              ]}
+              category={"tops"}
+              onSelect={this.onSelect}
+            />
+          </div>
+          <H3 font="secondary">Which bottom best represents your style?</H3>
+          <div style={{ margin: "2em", display: "flex", flexDirection: "row" }}>
+            <SurveyCards
+              images={[
+                { id: "casual", src: "red" },
+                { id: "classy", src: "red" },
+                { id: "sketchy", src: "red" },
+              ]}
+              category={"bottoms"}
+              onSelect={this.onSelect}
+            />
+          </div>
+          <H3 font="secondary">Which accecories best represent your style?</H3>
+          <div style={{ margin: "2em", display: "flex", flexDirection: "row" }}>
+            <SurveyCards
+              images={[
+                { id: "casual", src: "red" },
+                { id: "classy", src: "red" },
+                { id: "sketchy", src: "red" },
+              ]}
+              category={"accessories"}
+              onSelect={this.onSelect}
+            />
+          </div>
+          <H3 font="secondary">Which 'something' best represent your style?</H3>
+          <div style={{ margin: "2em", display: "flex", flexDirection: "row" }}>
+            <SurveyCards
+              images={[
+                { id: "casual", src: "red" },
+                { id: "classy", src: "red" },
+                { id: "sketchy", src: "red" },
+              ]}
+              category={"something"}
+              onSelect={this.onSelect}
+            />
+          </div>
+
+          <Button type="button" theme="primary" onClick={() => this.onSubmit()}>
+            <Icon size={1.2} style={{ color: white }}>
+              add
+            </Icon>{" "}
+            Submit Survey
+          </Button>
+        </div>
       </section>
     );
   }
 }
 
 export default StylePreferences;
-
--- code/web/src/modules/user/SurveyCards.js
+++ code/web/src/modules/user/SurveyCards.js
@@ -1,19 +1,28 @@
 import Card from "../../ui/card/Card";
-import { white} from "../../ui/common/colors";
+import { white } from "../../ui/common/colors";
 import React from "react";
 
 const SurveyCards = (props) => {
-  let surveyOptions = props.props.map((image) => {
+  let surveyOptions = props.images.map((image) => {
     return (
-      <Card style={{ width: "18em", backgroundColor: white }}>
-        <p style={{ padding: "2em 3em 0 3em" }}>
-          <img src={``} alt={image} style={{ width: "100%" }} />
-        </p>
-      </Card>
+      <button
+        type="button"
+        onClick={() => props.onSelect(image.id, props.category)}
+      >
+        <Card style={{ width: "18em", backgroundColor: white }}>
+          <p style={{ padding: "2em 3em 0 3em" }}>
+            <img
+              src={image.src}
+              alt={image.id}
+              style={{ width: "100%" }}
+            />
+          </p>
+        </Card>
+      </button>
     );
   });
 
   return <section>{surveyOptions}</section>;
 };
 
-export default StyleQuestion;
+export default SurveyCards;
