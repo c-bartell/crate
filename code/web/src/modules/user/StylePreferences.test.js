94ba6283f7e5ae79aaf6fdb61553c0eaa169f249
Bryce Mara Jarrett <brycej98@gmail.com>
2021-02-13 12:19:37 -0600

    Add sad path test for submit button

--- code/web/src/modules/user/StylePreferences.test.js
+++ code/web/src/modules/user/StylePreferences.test.js
@@ -1,12 +1,9 @@
 import React from "react";
 import StylePreferences from "./StylePreferences";
-import { screen, render } from "@testing-library/react";
+import { screen, render, fireEvent } from "@testing-library/react";
 import "@testing-library/jest-dom";
-// import userEvent from "@testing-library/user-event";
-import { MemoryRouter } from "react-router-dom";
 
 describe("StylePreferences", () => {
-
   it("should render header correctly", () => {
     render(<StylePreferences />);
     expect(screen.getByText("Style Survey")).toBeInTheDocument();
@@ -14,10 +11,42 @@ describe("StylePreferences", () => {
 
   it("should render questions correctly", () => {
     render(<StylePreferences />);
-    expect(screen.getByText("Favorite vacation destination?")).toBeInTheDocument();
-    expect(screen.getByText("Are you more of a spicy, sweet or sour person?")).toBeInTheDocument();
-    expect(screen.getByText("You're hungry, what restaurant do you go to?")).toBeInTheDocument();
-    expect(screen.getByText("Which would you pick as your dream home?")).toBeInTheDocument();
+    expect(
+      screen.getByText("Favorite vacation destination?")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Are you more of a spicy, sweet or sour person?")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("You're hungry, what restaurant do you go to?")
+    ).toBeInTheDocument();
+    expect(
+      screen.getByText("Which would you pick as your dream home?")
+    ).toBeInTheDocument();
   });
 
+  it("should be able to click submit if images have been selected", () => {
+    render(<StylePreferences />);
+    fireEvent.click(
+      screen.getByAltText("/images/stock/survey/mountains-A.jpg")
+    );
+    fireEvent.click(screen.getByAltText("/images/stock/survey/spicy-B.jpg"));
+    fireEvent.click(screen.getByAltText("/images/stock/survey/burger-C.jpg"));
+    fireEvent.click(
+      screen.getByAltText("/images/stock/survey/apartment-D.jpg")
+    );
+
+    fireEvent.click(screen.getByText("Submit Survey"));
+
+    // expect the modal to show up with the style summary
+  });
+
+  it("should not be able to click submit if no images have been selected", () => {
+    window.alert = jest.fn();
+    render(<StylePreferences />);
+
+    fireEvent.click(screen.getByText("Submit Survey"));
+
+    expect(window.alert).toHaveBeenCalled()
+  });
 });
